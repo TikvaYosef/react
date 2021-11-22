694c227849eaf378ebb858d1248f362c1abfe61d
TikvaYosef <tikvayosef13@gmail.com>
2021-11-22 09:57:52 +0200

    commit

--- main.js
+++ main.js
@@ -1,21 +1,2 @@
 
-// const name = " Amir"
-// const name2 = "Tikva"
-
-
-// function callName() {
-
-//     console.log(name);
-//     setTimeout(() => { console.log("this is the 5 delay message") }, 5000);
-//     console.log(name2);
-//     console.log(name);
-//     console.log(name2);
-//     setTimeout(() => { console.log("this is the 2 delay message") }, 1000);
-//     console.log(name);
-//     setTimeout(() => { console.log("this is the 0 delay message") }, 0);
-//     console.log(name);
-//     setTimeout(() => { console.log("this is the 3 delay message") }, 3000);
-
-// }
-// callName()
 
